af3da81a7077693fb94fd7d3d1697d868fcf4d06
ST-byte441 <seungtaekoh5@gmail.com>
2025-07-12 12:59:56 -0700

    feat: 1/2 of screenAudio in Input

--- src/test/index.ts
+++ src/test/index.ts
@@ -2,9 +2,9 @@ import Waviz from '../primary/waviz';
 
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const audio = document.getElementById('audio') as HTMLAudioElement;
-const wavizTest = new Waviz(canvas, audio);
+// const wavizTest = new Waviz(canvas, audio);
 // const wavizTest = new Waviz(canvas, 'microphone');
-// const wavizTest = new Waviz(canvas, 'screenAudio');
+ const wavizTest = new Waviz(canvas, 'screenAudio');
 
 // Testing option objects
 const optionsWave = { lineWidth: 3, lineColor: 'blue', multipliyer: 3 };
